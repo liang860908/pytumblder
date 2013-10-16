bf7f708c63816a11c08697af7cf1aff18e4ee87f
Florent Peterschmitt <florent@peterschmitt.fr>
2013-10-16 22:45:56 +0000

    better regex

--- tumblder/regex.py
+++ tumblder/regex.py
@@ -7,9 +7,9 @@ from re import compile as rec
 
 _PICTYPES = '|'.join(['png', 'jpg', 'jpeg', 'gif'])
 
-PHOTO = rec('(http[s]?://[a-z0-9\.]*(?:media|static).tumblr.com/(?!avatar|preview).*\.(?:' + _PICTYPES + '))', re.I)
-PHOTOSET = rec('(http[s]?://.*/photoset_iframe/.*/false)', re.I)
-STATICPHOTO = rec('.*static.tumblr.com.*', re.I)
+PHOTO = rec('(http[s]?://[a-z0-9\.]*(?:media|static).tumblr.com/(?!avatar|preview)[^ ]*\.(?:' + _PICTYPES + '))', re.I)
+PHOTOSET = rec('(http[s]?://[^ ]*/photoset_iframe/[^ ]*/false)', re.I)
+STATICPHOTO = rec('[^ ]*static.tumblr.com[^ ]*', re.I)
 SIZEDPHOTO = rec('.*/(.*_)([0-9]+)\.(' + _PICTYPES + ')', re.I)
 
 FILENAME = rec('http[s]?://.*/(.*\..*)')
