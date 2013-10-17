104c95a937846d4ffc546131d922169171c84953
Florent Peterschmitt <florent@peterschmitt.fr>
2013-10-17 14:59:35 +0000

    options help, remove the --recursive option

--- run.py
+++ run.py
@@ -6,20 +6,25 @@ import tumblder.download
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-b', '--blog', required=True)
-    parser.add_argument('-f', '--dldir', default='./')
-    parser.add_argument('-l', '--pagelimit', default=1, type=int)
-    parser.add_argument('-o', '--open', action='store_true')
-    parser.add_argument('-p', '--openin', default='firefox ')
-    parser.add_argument('-r', '--recursive', action='store_true')
-    parser.add_argument('-s', '--smallsizes', action='store_true')
-    parser.add_argument('-u', '--forceupdate', action='store_true')
-    parser.add_argument('-S', '--startpage', default=1, type=int)
-    parser.add_argument('-V', '--videos', action='store_true')
+    parser.add_argument('-b', '--blog', required=True,
+            help='full url to tumblr blog. http://blog.tumblr.com')
+    parser.add_argument('-f', '--dldir', default='./',
+            help='download directory')
+    parser.add_argument('-l', '--pagelimit', default=1, type=int,
+            help='number of pages to watch')
+    parser.add_argument('-o', '--open', action='store_true',
+            help='open in a program. change with --openin')
+    parser.add_argument('-p', '--openin', default='firefox ',
+            help='open in this program')
+    parser.add_argument('-s', '--smallsizes', action='store_true',
+            help='take small and full size photos')
+    parser.add_argument('-u', '--forceupdate', action='store_true',
+            help='look each page for un update, instead of stopping on the first already-downloaded media')
+    parser.add_argument('-S', '--startpage', default=1, type=int,
+            help='start browsing from this page number')
+    parser.add_argument('-V', '--videos', action='store_true',
+            help='download videos too')
     args = parser.parse_args()
-
-    if args.recursive:
-        print('/!\\ recursive download (take reblogs) not implemented /!\\')
 
     res = tumblder.download.browse(args)
     if res == 1:
