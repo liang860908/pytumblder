ec1fd286b2f9e17b8c5761740fce72e513a26432
Florent Peterschmitt <florent@peterschmitt.fr>
2013-10-20 22:04:17 +0000

    another fix

--- tumblder/worker.py
+++ tumblder/worker.py
@@ -42,7 +42,7 @@ def getmedia(dldir, media):
             retry_dl = False
         except requests.exceptions.ConnectionError:
             retry_try += 1
-            print_log('download stalled {0}/{1}:'.format(retry_try, retry_max), media, True)
+            print_log('download stalled {0}/{1}:'.format(retry_try, retry_max), media['url'], True)
             time.sleep(10 * retry_try)
     if not retry_try < retry_max:
         print_log('download failed: ', media['url'], True)
