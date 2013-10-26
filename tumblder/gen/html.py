b73db3cd966e35bc446bdc4bf38006d75c200feb
Florent Peterschmitt <florent@peterschmitt.fr>
2013-10-26 20:52:04 +0000

    navigator on top and bottom

--- tumblder/gen/html.py
+++ tumblder/gen/html.py
@@ -5,10 +5,17 @@ import os
 from tumblder import regex
 
 def generate(directory, root=''):
+    pages = []
+    files = sorted(os.listdir(directory))
+    photos = [x for x in files if regex.ISPHOTO.match(x)]
+    photos_per_page = 8
+    pagenum = 0
+    pagemax = int(len(photos) / photos_per_page) - 1
+    imgwidth = str(int(100 / (photos_per_page / 2 + 1)))
+
     css = '''<style media="screen" type="text/css">
     img {
-        max-width: 300px;
-        max-height: 300px;
+        width: ''' + imgwidth + '''%;
     }
     p {
         margin: auto;
@@ -17,30 +24,31 @@ def generate(directory, root=''):
     #navigator {
         margin-bottom: 10px;
     }
+    #photos {
+        max-height: 90%;
+    }
     </style>'''
-    pages = []
-    files = sorted(os.listdir(directory))
-    photos = [x for x in files if regex.ISPHOTO.match(x)]
-    photos_per_page = 10
-    pagenum = 0
-    pagemax = int(len(photos) / photos_per_page) - 1
+
     for i in range(0, len(photos), photos_per_page):
-        photoset = photos[i:(i+10)]
+        photoset = photos[i:(i + photos_per_page)]
         precpage = pagenum - 1 if i > 0 else 0
         nextpage = pagenum + 1 if pagenum < pagemax else pagemax
         html = '<!DOCTYPE html>\n'
         html += '<html><head>{0}<meta charset="utf-8"></head><body>\n'.format(css)
-        html += '<p id="navigator">'
-        html += '<a href="index0.html">Début</a>'
-        html += '— <a href="index{0}.html">précédent</a>'.format(precpage) 
-        html += ' | '
-        html += '<a href="index{0}.html">suivant</a> —'.format(nextpage) 
-        html += '<a href="index{0}.html">Fin</a>'.format(pagemax)
-        html += '</p><p>\n'
+        nav = '<p id="navigator">'
+        nav += '<a href="index0.html">Début</a> '
+        nav += '— <a href="index{0}.html">précédent</a> | '.format(precpage) 
+        nav += '<a href="index{0}.html">suivant</a> — '.format(nextpage) 
+        nav += '<a href="index{0}.html">Fin</a>'.format(pagemax)
+        nav += '</p>\n'
+        html += nav
+        html += '<p id="photos">\n'
         for photo in photoset:
             src = os.path.join(root, photo)
-            html += '<a href="{0}"><img src="{0}" /></a>\n'.format(src)
-        html += '</p></body></html>'
+            html += '<a href="{0}"><img src="{0}"></a>\n'.format(src)
+        html += '</p>\n'
+        html += nav
+        html += '</body></html>'
         pages.append(html)
         pagenum += 1
     return pages
