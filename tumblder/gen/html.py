d89893c005714f982bdb2000564b76dd2a587d5a
Florent Peterschmitt <florent@peterschmitt.fr>
2013-10-26 20:21:58 +0000

    computing nextpage value. Alphachronological sort order.

--- tumblder/gen/html.py
+++ tumblder/gen/html.py
@@ -19,7 +19,7 @@ def generate(directory, root=''):
     }
     </style>'''
     pages = []
-    files = reversed(sorted(os.listdir(directory)))
+    files = sorted(os.listdir(directory))
     photos = [x for x in files if regex.ISPHOTO.match(x)]
     photos_per_page = 10
     pagenum = 0
@@ -27,16 +27,16 @@ def generate(directory, root=''):
     for i in range(0, len(photos), photos_per_page):
         photoset = photos[i:(i+10)]
         precpage = pagenum - 1 if i > 0 else 0
-        nextpage = pagenum + 1 if i < pagemax else pagemax
+        nextpage = pagenum + 1 if pagenum < pagemax else pagemax
         html = '<!DOCTYPE html>\n'
-        html += '<html><head>{0}<meta charset="utf-8"></head><body>'.format(css)
+        html += '<html><head>{0}<meta charset="utf-8"></head><body>\n'.format(css)
         html += '<p id="navigator">'
         html += '<a href="index0.html">Début</a>'
         html += '— <a href="index{0}.html">précédent</a>'.format(precpage) 
         html += ' | '
         html += '<a href="index{0}.html">suivant</a> —'.format(nextpage) 
         html += '<a href="index{0}.html">Fin</a>'.format(pagemax)
-        html += '</p><p>'
+        html += '</p><p>\n'
         for photo in photoset:
             src = os.path.join(root, photo)
             html += '<a href="{0}"><img src="{0}" /></a>\n'.format(src)
